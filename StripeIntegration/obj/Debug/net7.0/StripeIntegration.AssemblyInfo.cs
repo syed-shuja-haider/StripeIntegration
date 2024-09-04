4da6a8580188812b83ac26b32268320ec79ccabd
Syed Shuja Haider Sharazi <shuja.haider@markhortechnologies.com>
2024-09-04 20:17:22 +0500

    Updated Secrets

--- StripeIntegration/obj/Debug/net7.0/StripeIntegration.AssemblyInfo.cs
+++ StripeIntegration/obj/Debug/net7.0/StripeIntegration.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("StripeIntegration")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2cf562aaa4c57a33a28bf9514972b8db542eaad1")]
 [assembly: System.Reflection.AssemblyProductAttribute("StripeIntegration")]
 [assembly: System.Reflection.AssemblyTitleAttribute("StripeIntegration")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
